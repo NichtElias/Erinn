0b660e8c171cd8bf55921e06c897894f68b54a23
NichtElias <eliasnie@t-online.de>
2026-06-14 18:25:02 +0200

    remove old gradle tasks for pst tuning

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,18 +33,6 @@ tasks.shadowJar {
     }
 }
 
-tasks.register<ShadowJar>("tunePstJar") {
-    group = "build"
-    archiveClassifier.set("tunepst")
-
-    from(sourceSets.main.get().output)
-    configurations = listOf(project.configurations.runtimeClasspath.get())
-
-    manifest {
-        attributes["Main-Class"] = "party.elias.tunepst.TunePSTKt"
-    }
-}
-
 tasks.register<JavaExec>("run") {
     group = "application"
     description = "Runs the chess engine"
@@ -58,21 +46,6 @@ tasks.register<JavaExec>("run") {
     standardInput = System.`in`
     standardOutput = System.out
     errorOutput = System.err
-}
-
-tasks.register<JavaExec>("tunePst") {
-    group = "application"
-    description = "Tune the piece square tables"
-
-    classpath(sourceSets.main.get().runtimeClasspath)
-
-    mainClass.set("party.elias.tunepst.TunePSTKt")
-
-    standardInput = System.`in`
-    standardOutput = System.out
-    errorOutput = System.err
-
-    jvmArgs("-Xmx12G")
 }
 
 
