80446eebd447d4f8ae5b071af01df8fd2d1dcccd
NichtElias <eliasnie@t-online.de>
2026-08-15 15:00:05 +0200

    adjust hash size for sprt
    For as low of a time control as we're using, 32MiB hash size is a bit large, so it was adjusted to a more reasonable 8MiB.

--- sprt.sh
+++ sprt.sh
@@ -9,7 +9,7 @@ args=(
   -sprt elo0=0 elo1=10 alpha=0.05 beta=0.1
   -autosaveinterval 0 -config outname=fastchess-config.json
 #  -log engine=true file=fastchess.log
-  -each cmd="$java_path" tc=4+0.04 option.Hash=32
+  -each cmd="$java_path" tc=4+0.04 option.Hash=8
   -engine name="$1" args="-Xmx300M -jar $testbench/Erinn-$1.jar"
   -engine name="$2" args="-Xmx300M -jar $testbench/Erinn-$2.jar"
 )
