86aa9a884b377246f84f08f6e5cb0545aa5ff84a
NichtElias <eliasnie@t-online.de>
2026-08-21 20:06:00 +0200

    adjudicate in SPRT

--- sprt.sh
+++ sprt.sh
@@ -9,6 +9,8 @@ args=(
   -sprt elo0=0 elo1=10 alpha=0.05 beta=0.1
   -autosaveinterval 0 -config outname=fastchess-config.json
 #  -log engine=true file=fastchess.log
+  -draw movenumber=40 movecount=10 score=15
+  -resign movecount=4 score=700
   -each cmd="$java_path" tc=4+0.04 option.Hash=8
   -engine name="$1" args="-Xmx300M -jar $testbench/Erinn-$1.jar"
   -engine name="$2" args="-Xmx300M -jar $testbench/Erinn-$2.jar"
